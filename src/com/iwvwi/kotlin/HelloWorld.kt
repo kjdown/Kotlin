fd329a087d82ae2e9cb21a7cb0a2118f23245f3f
iwvwi <i@iwvwi.com>
2018-12-05 08:47:55 +0800

    KotlinHelloWorlds

--- src/com/iwvwi/kotlin/HelloWorld.kt
+++ src/com/iwvwi/kotlin/HelloWorld.kt
@@ -1,6 +1,10 @@
 package com.iwvwi.kotlin
 
+fun sum (a : Int ,b: Int ) : Int {
+    return a+b
+}
+
 fun main(args:Array<String>){
-    println("hello...")
-    println("")
+    print("sum is = ")
+    println(sum(88,99))
 }
